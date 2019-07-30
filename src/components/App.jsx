da40d264a8fb63d2690f51e2940b58838bfcddc6
vivxz <viviannquan@gmail.com>
2019-07-29 19:35:29 -0700

    Completed up to VideoListEntry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,54 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
+import Search from './Search.js';
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+
+var App = props => (
+<div>
+  <nav className="navbar">
+    <div className="col-md-6 offset-md-3">
+      <div><h5><em>search</em> view goes here</h5></div>
+    </div>
+  </nav>
+  <div className="row">
+    <div className="col-md-7">
+      <div><h5><em>videoPlayer</em> view goes here</h5></div>
+    </div>
+    <div className="col-md-5">
+      <div><h5><em>videoList</em><VideoList videos={props.data}/></h5></div>
     </div>
   </div>
+</div>
+
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state{
+
+//     }
+//   }
+//   render() {
+
+//   }
+//   return(
+//       <div>
+//   <nav className="navbar">
+//     <div className="col-md-6 offset-md-3">
+//       <div><h5><em>search</em> view goes here</h5></div>
+//     </div>
+//   </nav>
+//   <div className="row">
+//     <div className="col-md-7">
+//       <div><h5><em>videoPlayer</em> view goes here</h5></div>
+//     </div>
+//     <div className="col-md-5">
+//       <div><h5><em>videoList</em> <VideoList /></h5></div>
+//     </div>
+//   </div>
+//   </div >
+//     )
+//   }
 );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,19 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+// import exampleVideoData from '../data/exampleVideoData.js';
+
+var VideoList = props => (
+  // <div className="video-list">
+  //   <div><h5><em>videoListEntry</em> {props.videos.snippet}</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  // </div>
+
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  {props.videos.map(video => (
+    <VideoListEntry video={video} />
+  ))}
   </div>
 );
 
